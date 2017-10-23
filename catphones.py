e7d55a34bc401758e028b63fbb282953559dea5d
collinRawrXD <32073358+collinRawrXD@users.noreply.github.com>
2017-10-23 13:32:07 -0400

    Comments
    Added comments to catphones class

--- catphones.py
+++ catphones.py
@@ -5,18 +5,20 @@ class CatPhones():
         self.color = color
         self.music = music #0 or 1
         self.musicOff = musicOff #true false
-        self.musicOff = True
+        self.musicOff = True #Why do you take a parameter and then force it to true?
         self.isOn = False
-        self.song = song
+        self.song = song #Is this a number?
 
 
     def get_interface(self, heldItems,current_room):
         if self.isOn and self.musicOff:
             print("The "+self.color+" cat headphones are swithced on and not playing music "+self.color+" You can TURN "+self.color.upper()+" CAT HEADPHONE MUSIC ON")
+            #..."not playing music blue You can TURN BLUE CAT HEADPHONE MUSIC ON"
+            #second self.color is out of place, TURN [color] CAT HEADPHONE MUSIC ON isn't recognized as a command
         elif self.isOn and not self.musicOff:
             print("The "+self.color+" cat headphones are swithced on, and glowing neon "+self.color.upper()+" you hear some music so you put the headphones on. You can TURN MUSIC OFF")
         else:
-            print("The "+self.color+" cat headphones are swithced off. You can PRESS THE ON BUTTON")
+            print("The "+self.color+" cat headphones are swithced off. You can PRESS THE ON BUTTON") #PRESS THE ON BUTTON isn't included in check_input
         if self.music == 0 and "charger" in heldItems:
             print("You can PRESS THE PLAY BUTTON.")
         if self.music > 0:
@@ -25,7 +27,7 @@ class CatPhones():
     def check_input(self,command,heldItems,current_room):
         if command == "PRESS THE PLAY BUTTON":
             self.turn_on()
-            self.get_song()
+            self.get_song() #This method does not exist- and what are you doing with the song name?
         if command == "TURN MUSIC OFF":
             self.turn_off()
             
@@ -37,15 +39,16 @@ class CatPhones():
             self.color = 0
 
     def getSong(self):
-        return songList(self.song)
+        #return songList(self.song)
+        return CatPhones.songList[self.song] #fixed
 
     #Mutators
     def turn_on(self):
-        if not self.isOn:
+        if not self.isOn: #This doesn't do anything
             
         if self.music == 1 and not self.musicOff:
             self.isOn = True
-            print("You click the play button and music starts playing. You are listenting to "+self.getNextColor() )
+            print("You click the play button and music starts playing. You are listenting to "+self.getNextColor() ) #self.getNextColor() does not exist
         else:
             print("You click the play button but nothing happens. Maybe you need some music?")
 
